fc722230899f15e70a5ab02968a51fdc62a80266
Carlos Pessin <162637486+Carlos-Pessin@users.noreply.github.com>
2024-09-24 16:53:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import utils
 import pandas as pd
 
 # Page config
-st.set_page_config(page_title='Costumer clustering')
-st.title('Costumer clustering and business strategy')
+st.set_page_config(page_title='Custumer clustering')
+st.title('Custumer clustering and business strategy')
 
 st.markdown(
     """
 ## Customer Clustering
-From the database, customers were grouped according to the FM metrics:
+Customers were grouped according to the FM metrics:
 - F: Frequency = number of purchases made by the customer
 - M: Mean Ticket = average value of the customer's purchases
 
@@ -22,14 +22,39 @@ st.image('data/clusters.png')
 
 st.markdown(
     """
-The main characteristics of the groups are:
-- 0: Customers who purchase with higher Frequency but lower Mean Ticket;
-- 1: Customers who purchase with higher Mean Ticket but low Frequency;
-- 2: Customers who purchase with low Frequency and Mean Ticket.
 
-Another approach consisted of grouping customers according to the items they purchased, that is, customers who bought more similar products were grouped.
+Another approach consisted of grouping customers according to the items they purchased, that is, customers who bought more similar products were separated in 4 groups. This can't be seen in a chart but this information can be accessed.
 
-## Product Recomendation
+## Group analisys and business strategy
+
+The main characteristics of the 3 groups (from FM strategy) are:
+- 0:   Customers who purchase with higher Frequency but lower Mean Ticket;
+- 1:   Customers who purchase with higher Mean Ticket but low Frequency;
+- 2:   Customers who purchase with low Frequency and Mean Ticket.
+
+Having defined the 3 groups of clients, we can analyze their behavior and their impact on revenue, as we can see in the upcoming table:
+
+### Total Spent by customers from each group
+    """
+)
+
+cluster_analisys = pd.read_csv('data/cluster_analisys.csv').rename(columns={'Label': 'Group'})
+st.table(data=cluster_analisys)
+
+st.markdown(
+    """
+This analisys shows that the most profitable cluster is the one labeled 0, followed by clusters 1 and 2, respectively. In fact, cluster 0 clients spent 7 times more in average than cluster 1 and 14 times more than cluster 2 clients.
+
+Therefore, the company can increase its earnings by increasing frequency of purchases, to achieve this goal, it would be interesting for the company to set strategies to retain customers, even if it requires price reduction. 
+
+Another strategy is to implement a recomendation system to the company's website, as prototyped below.
+    """
+)
+
+
+st.markdown(
+    """
+## Product Recomendation Prototype
 
 Below you can generate product recommendations for a customer based on the consumption of other customers using the two methods defined above.
     """
@@ -47,30 +72,7 @@ if method == 'RFM':
 else:
     recomendations = utils.make_recomendation_byitens(client=client, n_itens=n_itens)
 
-st.success(f'These are the top {n_itens:.2f} recomendations')
+st.success(f'These are the top {int(n_itens):d} recomendation(s)')
 st.table(data=recomendations)
 
 
-st.markdown(
-    """
-## Group analisys and business strategy
-
-Having defined the 3 groups of clients, we can analyze their behavior and impact on revenue, as we can see in the upcoming table:
-
-### Total Spent by clients from each group
-    """
-)
-
-cluster_analisys = pd.read_csv('data/cluster_analisys.csv').rename(columns={'Label': 'Group'})
-
-st.table(data=cluster_analisys)
-
-st.markdown(
-    """
-This analisys shows that the most profitable cluster is the one labeled 0 (High Frequency and smaller Mean Ticket), followed by clusters 1 and 2. In fact, cluster 0 clients spent 7 times more in average than cluster 1 and 14 times more than cluster 2 clients.
-
-Therefore, the company can increase its earnings by increasing frequency of purchases, so it would be interest for the company to set strategies to retain customers, even if it requires price reduction. 
-
-Another strategy is to implement a recomendation system to the company's website, as described before.
-    """
-)
